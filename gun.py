bb81fe77bcb3e4f23ade2891c6b7da61458fed2c
NikP512 <npayusov1@gmail.com>
2023-11-21 20:46:27 +0300

    Все действия с игрой перенесены в класс Engine (кроме обработки попадания). Основная часть программы теперь только вызывает методы объекта engine.

--- gun.py
+++ gun.py
@@ -57,7 +57,6 @@ class Ball:
         if self.x >= WIDTH - self.r and self.vx > 0 or self.x <= self.r and self.vx < 0:
             self.vx = -self.vx//2
 
-
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
 
@@ -71,6 +70,7 @@ class Ball:
         """
         return (self.x - obj.x)**2 + (self.y - obj.y)**2 < (self.r + obj.r)**2
 
+
 class Gun:
     def __init__(self, screen):
         self.screen = screen
@@ -78,16 +78,6 @@ class Gun:
         self.an = 0
         self.growth_power = 1
         self.color = GREY
-
-    def get_on(self, keys):
-        if keys[pygame.K_SPACE]:
-            if self.power < 10 or self.power > 50:
-                self.growth_power = -self.growth_power
-            self.power_up()
-        if keys[pygame.K_w]:
-            self.targetting(1)
-        if keys[pygame.K_s]:
-            self.targetting(-1)
 
     def targetting(self, c):
         """Изменение угла выстрела. Увеличение происходит при нажатии 'w', уменьшение -- при нажатии 's'."""
@@ -102,6 +92,7 @@ class Gun:
         self.power += self.growth_power
 
     def get_off(self, event):
+        """"""
         if event.key == pygame.K_SPACE:
             global bullet
             bullet += 1
@@ -115,10 +106,10 @@ class Gun:
     def draw(self):
         pygame.draw.polygon(self.screen, self.color, [(0, 500), (self.power*math.cos(self.an/180*math.pi),500-self.power*math.sin(self.an/180*math.pi)), (10*math.sin(self.an/180*math.pi)+self.power*math.cos(self.an/180*math.pi), 500+10*math.cos(self.an/180*math.pi)-self.power*math.sin(self.an/180*math.pi)), (10*math.sin(self.an/180*math.pi), 500+10*math.cos(self.an/180*math.pi))])
 
+
 class Target:
     def __init__(self, screen):
         self.screen = screen
-        self.points = 0
         self.new_target()
 
     def new_target(self):
@@ -129,55 +120,66 @@ class Target:
         self.y = randint(self.r, 500)
         self.color = (RED, WHITE, BLACK)
 
-    def hit(self, points=1):
-        """Попадание шарика в цель."""
-        self.points += points
-
     def draw(self):
         pygame.draw.circle(self.screen, self.color[0], (self.x, self.y), self.r, width=self.r//2)
         pygame.draw.circle(self.screen, self.color[1], (self.x, self.y), self.r-self.r//2)
         pygame.draw.circle(self.screen, self.color[2], (self.x, self.y), 2)
 
-class Game_Engine:
+
+class GameEngine:
     """Игровой "движок", который следит за состоянием объектов и обновляет их."""
-    def __init__(self, screen):
-        self.screen = screen
-        self.gun = Gun(screen)
-        self.target = Target(screen)
+    def __init__(self):
+        pygame.init()
+        self.running = True
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.gun = Gun(self.screen)
+        self.target = Target(self.screen)
         self.balls = []
+
     def update(self):
-        screen.fill(LIGHT_ZEFIR)
+        self.screen.fill(LIGHT_ZEFIR)
         self.gun.draw()
         self.target.draw()
         for ball in self.balls:
             ball.draw()
+            ball.move()
+        pygame.display.update()
+        self.clock.tick(FPS)
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-bullet = 0
+    def shoot(self):
+        new_ball = Ball(self.screen)
+        new_ball.vx = self.gun.power * math.cos(self.gun.an / 180 * math.pi)
+        new_ball.vy = self.gun.power * math.sin(self.gun.an / 180 * math.pi)
+        engine.balls.append(new_ball)
+        self.gun.power = 10
+        self.gun.growth_power = 1
 
-clock = pygame.time.Clock()
-engine = Game_Engine(screen)
-finished = False
+    def checking_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                self.running = False
+            if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                self.shoot()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]:
+                if self.gun.power < 10 or self.gun.power > 50:
+                    self.gun.growth_power = -self.gun.growth_power
+                self.gun.power_up()
+            if keys[pygame.K_w]:
+                self.gun.targetting(1)
+            if keys[pygame.K_s]:
+                self.gun.targetting(-1)
 
-while not finished:
+
+engine = GameEngine()
+while engine.running:
     engine.update()
-    pygame.display.update()
-
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            finished = True
-        if event.type == pygame.KEYUP:
-            engine.gun.get_off(event)
-        keys = pygame.key.get_pressed()
-        engine.gun.get_on(keys)
+    engine.checking_events()
 
     for b in engine.balls:
-        b.move()
         if b.hittest(engine.target) and engine.target.live:
             engine.target.live = 0
             b.live = 0
-            engine.target.hit()
             engine.target.new_target()
 pygame.quit()
