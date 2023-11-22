c70672a363e128613685b4f51ddd1a854cfa71b9
NikP512 <npayusov1@gmail.com>
2023-11-23 02:43:38 +0300

    Добавлены бомбочки (пока ни на что не влияют), возвращена гравитация и удалеы шары, вылетающие за пределы экрана.

--- gun.py
+++ gun.py
@@ -8,22 +8,23 @@ FPS = 60
 
 RED = 0xFF0000
 BLUE = 0x0000FF
+LIGHT_BLUE = (66, 170, 255)
 YELLOW = 0xFFC91F
 GREEN = 0x00FF00
 DARK_GREEN = (0, 69, 36)
+HACKY = (128, 107, 42)
 MAGENTA = 0xFF03B8
 CYAN = 0x00FFCC
 BLACK = (0, 0, 0)
 WHITE = 0xFFFFFF
 GREY = 0x7D7D7D
 BROWN = (150, 75, 0)
-LIGHT_ZEFIR = (233, 216, 218)
 GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 WIDTH = 800
 HEIGHT = 600
 
-G = 0
+G = 1
 
 
 class Ball:
@@ -37,13 +38,6 @@ class Ball:
         self.color = choice(GAME_COLORS)
 
     def move(self):
-        """Переместить мяч по прошествии единицы времени.
-
-        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
-        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
-        и стен по краям окна (размер окна 800х600).
-        """
-
         self.x += self.vx
         self.y -= self.vy
         self.vy -= G
@@ -52,13 +46,6 @@ class Ball:
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
 
     def hittest(self, obj):
-        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
-
-        Args:
-            obj: Обьект, с которым проверяется столкновение.
-        Returns:
-            Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
-        """
         return (self.x - obj.x)**2 + (self.y - obj.y)**2 < (self.r + obj.r)**2
 
 
@@ -66,12 +53,14 @@ class Gun:
     def __init__(self, screen, x, y, place="left"):
         self.screen = screen
         self.place = place
+        self.frozen = False
+        self.growth_power = 1
         self.power = 10
         self.an = 20 if self.place == "left" else 160
         self.x = x
         self.y = y - 15
-        self.growth_power = 1
-        self.color = DARK_GREEN if self.place == "left" else RED
+        self.r = 15
+        self.color = (DARK_GREEN, HACKY) if self.place == "left" else (HACKY, DARK_GREEN)
 
     def move(self, c):
         self.x += c
@@ -87,7 +76,6 @@ class Gun:
                 self.x = WIDTH//2+50
 
     def targetting(self, c):
-        """Изменение угла выстрела. Увеличение происходит при нажатии 'w', уменьшение -- при нажатии 's'."""
         self.an += c
         if self.an > 160:
             self.an = 160
@@ -95,16 +83,12 @@ class Gun:
             self.an = 20
 
     def power_up(self):
-        """Выбор скорости выстрела. Происходит при удерживании пробела"""
         self.power += self.growth_power
         if self.power < 10 or self.power > 50:
             self.growth_power = -self.growth_power
     
     def draw(self):
-        pygame.draw.polygon(self.screen, self.color, [(self.x-30, self.y), (self.x+30, self.y),
-                                                      (self.x+20, self.y+15), (self.x-20, self.y+15)])
-        pygame.draw.circle(self.screen, self.color, (self.x, self.y), 15)
-        pygame.draw.polygon(self.screen, self.color,
+        pygame.draw.polygon(self.screen, self.color[0],
                             [(self.x-5*math.sin(math.radians(self.an)),
                               self.y-5*math.cos(math.radians(self.an))),
                              (self.x-5*math.sin(math.radians(self.an))+(30+self.power)*math.cos(math.radians(self.an)),
@@ -113,15 +97,18 @@ class Gun:
                               self.y+5*math.cos(math.radians(self.an))-(30+self.power)*math.sin(math.radians(self.an))),
                              (self.x+5*math.sin(math.radians(self.an)),
                               self.y+5*math.cos(math.radians(self.an)))])
+        pygame.draw.circle(self.screen, self.color[1], (self.x, self.y), self.r)
+        pygame.draw.polygon(self.screen, self.color[0], [(self.x-30, self.y), (self.x+30, self.y),
+                                                      (self.x+20, self.y+15), (self.x-20, self.y+15)])
 
 
 class StoppedTarget:
     def __init__(self, screen):
         self.screen = screen
         self.r = randint(10, 30)
-        self.left_points = 50 - self.r
-        self.right_points = 50 - self.r
-        self.x = randint(100, WIDTH - 100)
+        self.left_points = 40 - self.r
+        self.right_points = 40 - self.r
+        self.x = randint(50, WIDTH - 50)
         self.y = randint(100, HEIGHT - 100)
         self.color = (RED, WHITE, BLACK)
 
@@ -135,9 +122,9 @@ class MovedTarget:
     def __init__(self, screen):
         self.screen = screen
         self.r = randint(10, 30)
-        self.left_points = 50 - self.r
-        self.right_points = 50 - self.r
-        self.x = randint(100, WIDTH - 100)
+        self.left_points = 70 - self.r
+        self.right_points = 70 - self.r
+        self.x = randint(50, WIDTH - 50)
         self.y = randint(100, HEIGHT - 100)
         self.vx = randint(-10, 10)
         self.vy = randint(-10, 10)
@@ -151,14 +138,61 @@ class MovedTarget:
     def move(self):
         self.x += self.vx
         self.y -= self.vy
-        if (self.x < 100 and self.vx < 0) or (self.x > WIDTH - 100 and self.vx > 0):
+        if (self.x < self.r and self.vx < 0) or (self.x > WIDTH - self.r and self.vx > 0):
             self.vx = -self.vx
-        if (self.y < 100 and self.vy > 0) or (self.y > HEIGHT - 100 and self.vy < 0):
+        if (self.y < 50 and self.vy > 0) or (self.y > HEIGHT - 50 - self.r and self.vy < 0):
             self.vy = -self.vy
 
 
+class RandomTarget:
+    def __init__(self, screen):
+        self.screen = screen
+        self.r = randint(10, 30)
+        self.left_points = 50 - self.r
+        self.right_points = 50 - self.r
+        self.x = randint(50, WIDTH - 50)
+        self.y = randint(100, HEIGHT - 100)
+        self.vx = randint(-10, 10)
+        self.vy = randint(-10, 10)
+        self.color = (YELLOW, WHITE, BLACK)
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color[0], (self.x, self.y), self.r, width=self.r//2)
+        pygame.draw.circle(self.screen, self.color[1], (self.x, self.y), self.r-self.r//2)
+        pygame.draw.circle(self.screen, self.color[2], (self.x, self.y), 2)
+
+    def move(self):
+        self.x += self.vx
+        self.y -= self.vy
+        self.vx = randint(-10, 10)
+        self.vy = randint(-10, 10)
+        if (self.x < self.r and self.vx < 0) or (self.x > WIDTH - self.r and self.vx > 0):
+            self.vx = -self.vx
+        if (self.y < 50 and self.vy > 0) or (self.y > HEIGHT - 50 - self.r and self.vy < 0):
+            self.vy = -self.vy
+
+
+class Bomb:
+    def __init__(self, screen):
+        self.screen = screen
+        self.color = BLACK
+        self.x = randint(50, WIDTH - 50)
+        self.y = 50
+        self.r = 5
+        self.vy = 0
+
+    def move(self):
+        self.y -= self.vy
+        self.vy -= G
+
+    def hittest(self, obj):
+        return (self.x - obj.x)**2 + (self.y - obj.y)**2 < (self.r + obj.r)**2
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
+
+
 class GameEngine:
-    """Игровой "движок", который следит за состоянием объектов и обновляет их."""
     def __init__(self):
         pygame.init()
         self.left_points = 0
@@ -166,69 +200,57 @@ class GameEngine:
         self.running = True
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
+        self.cadr = 0
         self.left_gun = Gun(self.screen, 50, HEIGHT-50, "left")
         self.left_events = [False, False, False, False, False]
         self.right_gun = Gun(self.screen, WIDTH - 50, HEIGHT-50, "right")
         self.right_events = [False, False, False, False, False]
         self.target1 = StoppedTarget(self.screen)
         self.target2 = MovedTarget(self.screen)
+        self.target3 = RandomTarget(self.screen)
         self.left_balls = []
         self.right_balls = []
-        self.fonts = [pygame.font.SysFont('timesnewroman', 30), pygame.font.SysFont('timesnewroman', 20)]
+        self.bombs = []
+        self.font = pygame.font.SysFont('timesnewroman', 20)
 
     def draw(self):
-        """Метод, описывающий отрисовку объектов"""
-        self.screen.fill(LIGHT_ZEFIR)
+        self.screen.fill(LIGHT_BLUE)
         pygame.draw.rect(self.screen, BROWN, (0, HEIGHT-50, WIDTH, 50))
         self.target1.draw()
         self.target2.draw()
+        self.target3.draw()
         for ball in self.left_balls:
             ball.draw()
         for ball in self.right_balls:
             ball.draw()
+        for bomb in self.bombs:
+            bomb.draw()
         self.left_gun.draw()
         self.right_gun.draw()
-        self.screen.blit(self.fonts[0].render("Количество очков:" + str(self.left_points), True, BLACK), (10, 10))
-        self.screen.blit(self.fonts[1].render("Количество очков в раунде:" + str(self.target1.left_points+self.target2.left_points), True, BLACK), (10, 50))
-        self.screen.blit(self.fonts[0].render("Количество очков:" + str(self.right_points), True, BLACK), (WIDTH-300, 10))
-        self.screen.blit(self.fonts[1].render("Количество очков в раунде:" + str(self.target1.right_points+self.target2.right_points), True, BLACK), (WIDTH-300, 50))
+        self.screen.blit(self.font.render("Количество очков:" + str(self.left_points), True, BLACK), (10, 10))
+        self.screen.blit(self.font.render("Количество очков:" + str(self.right_points), True, BLACK), (WIDTH//2, 10))
         pygame.display.update()
         self.clock.tick(FPS)
 
     def left_shoot(self):
-        """Метод описывает поведение игры при выстреле: создается новый шар, обновляются атрибуты объекта engine."""
         new_ball = Ball(self.screen, self.left_gun.x, self.left_gun.y)
         new_ball.vx = self.left_gun.power * math.cos(self.left_gun.an / 180 * math.pi)
         new_ball.vy = self.left_gun.power * math.sin(self.left_gun.an / 180 * math.pi)
-        engine.left_balls.append(new_ball)
-        self.target1.left_points -= 5
-        self.target2.left_points -= 5
-        if self.target1.left_points < 0:
-            self.target1.left_points = 0
-        if self.target2.left_points < 0:
-            self.target2.left_points = 0
+        self.left_balls.append(new_ball)
+        self.left_points -= 5
         self.left_gun.power = 10
         self.left_gun.growth_power = 1
-        self.left_gun.color = DARK_GREEN
 
     def right_shoot(self):
-        """Метод описывает поведение игры при выстреле: создается новый шар, обновляются атрибуты объекта engine."""
         new_ball = Ball(self.screen, self.right_gun.x, self.right_gun.y)
         new_ball.vx = self.right_gun.power * math.cos(self.right_gun.an / 180 * math.pi)
         new_ball.vy = self.right_gun.power * math.sin(self.right_gun.an / 180 * math.pi)
-        engine.right_balls.append(new_ball)
-        self.target1.right_points -= 5
-        self.target2.right_points -= 5
-        if self.target1.right_points < 0:
-            self.target1.right_points = 0
-        if self.target2.right_points < 0:
-            self.target2.right_points = 0
+        self.right_balls.append(new_ball)
+        self.right_points -= 5
         self.right_gun.power = 10
         self.right_gun.growth_power = 1
-        self.right_gun.color = RED
 
     def checking_events(self):
-        """Метод проверяет события, произошедшие за один кадр и вызывает соответствующие методы своих атрибутов."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 self.running = False
@@ -299,28 +321,65 @@ class GameEngine:
         if self.right_events[4]:
             self.right_gun.power_up()
 
+    def generate_bombs(self):
+        if self.cadr % 100 == 0:
+            self.bombs.append(Bomb(self.screen))
+
     def update(self):
+        self.cadr += 1
+        self.generate_bombs()
+        for bomb in self.bombs:
+            bomb.move()
+            if bomb.hittest(self.left_gun):
+                self.left_gun.frozen = True
+            if bomb.hittest(self.right_gun):
+                self.left_gun.frozen = True
+            if bomb.y > HEIGHT - 50:
+                self.bombs.remove(bomb)
         self.target2.move()
-        for ball in engine.left_balls:
+        self.target3.move()
+        for ball in self.left_balls:
             ball.move()
-            if ball.hittest(engine.target1):
+            if ball.hittest(self.target1):
                 self.left_points += self.target1.left_points
-                engine.target1 = StoppedTarget(engine.screen)
-                engine.left_balls.remove(ball)
-            if ball.hittest(engine.target2):
+                self.target1 = StoppedTarget(self.screen)
+                self.left_balls.remove(ball)
+            if ball.hittest(self.target2):
                 self.left_points += self.target2.left_points
-                engine.target2 = MovedTarget(engine.screen)
-                engine.left_balls.remove(ball)
-        for ball in engine.right_balls:
+                self.target2 = MovedTarget(self.screen)
+                self.left_balls.remove(ball)
+            if ball.hittest(self.target3):
+                self.left_points += self.target3.left_points
+                self.target3 = RandomTarget(self.screen)
+                self.left_balls.remove(ball)
+            if ball.hittest(self.right_gun):
+                self.right_gun.frozen = True
+                self.left_balls.remove(ball)
+            if (ball.x < 0 and ball.vx < 0) or (ball.x > WIDTH and ball.vx > 0):
+                self.left_balls.remove(ball)
+            if ball.y > HEIGHT - 50 and ball.vy < 0:
+                self.left_balls.remove(ball)
+        for ball in self.right_balls:
             ball.move()
-            if ball.hittest(engine.target1):
+            if ball.hittest(self.target1):
                 self.right_points += self.target1.right_points
-                engine.target1 = StoppedTarget(engine.screen)
-                engine.right_balls.remove(ball)
-            if ball.hittest(engine.target2):
+                self.target1 = StoppedTarget(self.screen)
+                self.right_balls.remove(ball)
+            if ball.hittest(self.target2):
                 self.right_points += self.target2.right_points
-                engine.target2 = MovedTarget(engine.screen)
-                engine.right_balls.remove(ball)
+                self.target2 = MovedTarget(self.screen)
+                self.right_balls.remove(ball)
+            if ball.hittest(self.target3):
+                self.right_points += self.target3.right_points
+                self.target3 = RandomTarget(self.screen)
+                self.right_balls.remove(ball)
+            if ball.hittest(self.left_gun):
+                self.left_gun.frozen = True
+                self.right_balls.remove(ball)
+            if (ball.x < 0 and ball.vx < 0) or (ball.x > WIDTH and ball.vx > 0):
+                self.right_balls.remove(ball)
+            if ball.y > HEIGHT - 50 and ball.vy < 0:
+                self.right_balls.remove(ball)
 
 
 engine = GameEngine()
